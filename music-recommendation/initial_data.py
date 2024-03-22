7ef22b320c60e1109a9056ce74160c96d301a782
San <santoshsahini@gmail.com>
2024-03-21 21:39:25 -0400

    audio features - V1

--- music-recommendation/initial_data.py
+++ music-recommendation/initial_data.py
@@ -27,16 +27,18 @@ def _get_playlist_data():
         # Using search request, get playlists data based on query
         result = sp.search(q="genre%telugu", offset=offset, type="playlist")
         total_number_of_playlists = result["playlists"]["total"]
+        #print(total_number_of_playlists)
 
         # For each playlist in search result, get the playlistID and append to a list
         for playlist in result["playlists"]["items"]:
             playlistIDs.append(playlist["id"])
             
         # get the last evaluated record since search request will only fetch 50 records at a time
-        offset += len(result["playlists"]["items"]) 
+        offset += len(result["playlists"]["items"])
+        #print("offset:",offset) 
         
         # TODO: update offset limit to total_number_of_playlists when getting the data
-        if offset >= 151: 
+        if offset >= 1: 
             break
     
     return playlistIDs
@@ -44,12 +46,22 @@ def _get_playlist_data():
 
 # Get list of tracks based on playlist ID
 def _get_tracks_list(playlist_id):
+    '''
+
+    This functions returns all the tracks present in the playlist using playlist_ID
+    output type: list[]
+    output format: list of dicts 
+    each dict contains 4 keys - track id, track name, album name, artist name
+
+    '''
     tracks_list = []
     offset = 0
     while (True):
+        # fetches all tracks available in the playlist
         tracks_api_resp = sp.playlist_tracks(playlist_id, offset=offset)
         total_tracks_in_playlist = tracks_api_resp['total']
 
+        # iterate over each track and retrieve meta data
         for item in tracks_api_resp['items']:
             if not item['track'] == None:
                 temp_dict = {
@@ -59,9 +71,10 @@ def _get_tracks_list(playlist_id):
                     "artist_name":item['track']['artists'][0]['name']
                 }
 
-                tracks_list.append(temp_dict)
+                tracks_list.append(temp_dict) #append each dict as an element in tracks list
         offset += len(tracks_api_resp['items'])
 
+        # condition to ensure all the tracks are retreived from the playlist
         if offset >= total_tracks_in_playlist:
             break
 
@@ -71,13 +84,15 @@ def _get_tracks_list(playlist_id):
 # Get audio features for the track
 def _get_audio_features(track_id):
     aud_features = sp.audio_features(track_id)
+    # audio = aud_features[0].keys()
 
     return aud_features
 
 
 
 def main():
-    # Retrieves all playlist IDs based on genre
+    
+    #Retrieves all playlist IDs based on genre
     playlist_ids = _get_playlist_data()
     # print(playlist_ids)
     print(f'fetched {len(playlist_ids)} playlists')
@@ -101,10 +116,21 @@ def main():
     print('-----')
     print(f'Unique tracks: {len(unique_tracks_data)}')
 
-    # Convert and write JSON object to file
-    with open("teluguTracks.json", "w") as outfile: 
-        json.dump(unique_tracks_data, outfile)
+    
 
+    # # Convert and write JSON object to file
+    # # with open("teluguTracks.json", "w") as outfile: 
+    # #     json.dump(unique_tracks_data, outfile)
+
+    # for i in range(len(unique_tracks_data)):
+
+    #     track_id = unique_tracks_data[i]["track_id"]
+    #     audio_features = _get_audio_features(track_id)
+
+    #     unique_tracks_data[0][i].update(audio_features)
+
+
+    print(_get_audio_features(["7MtXu7mXRMdICKyTOb8CuR","6FIaAGKucC6rgxpTGNLSBx"]))
     '''
     To do:
     - Get more than 100 songs from a playlist - done
